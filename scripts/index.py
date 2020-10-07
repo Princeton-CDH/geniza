151ba47fde13e0326da6c31cb4efc6246a349318
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-10-07 14:19:49 -0400

    Update indexing script to load CSV by url

--- scripts/index.py
+++ scripts/index.py
@@ -1,48 +1,59 @@
+import codecs
 import csv
 
 import click
 from flask import current_app
 from flask.cli import with_appcontext
+import requests
 
 from parasolr.solr.client import SolrClient
 
 
 @click.command()
-@click.argument('csvpath')
 @with_appcontext
-def index(csvpath):
-    print('Indexing %s' % csvpath)
+def index():
     solr = SolrClient(current_app.config['SOLR_URL'],
                       current_app.config['SOLR_CORE'])
 
     # clear the index in case any records have been removed or merged
     solr.update.delete_by_query('*:*')
 
-    with open(csvpath) as csvfile:
-        csvreader = csv.DictReader(csvfile)
-        rows = list(csvreader)
-        # check that pgp ids are unique
-        pgpids = [row['PGPID'] for row in rows]
-        if len(pgpids) != len(set(pgpids)):
-            print('Warning: PGPIDs are not unique!')
+    # load CSV data via URL
+    response = requests.get(current_app.config['METADATA_CSV_URL'],
+                            stream=True)
+    if response.status_code != requests.codes.ok:
+        print('Error accessing CSV: %s' % response)
+        # error code / exception ?
+        return
 
-        # index pgp data into Solr
-        solr.update.index([{
-            # use PGPID as Solr identifier
-            'id': row['PGPID'],
-            'description_txt': row['Description'],
-            'type_s': row['Type'],
-            'library_s': row['Library'],
-            'shelfmark_s': row['Shelfmark - Current'],
-            'shelfmark_txt': row['Shelfmark - Current'],
-            'tags_txt': [tag.strip() for tag in row['Tags'].split('#')],
-            'tags_ss': [tag.strip() for tag in row['Tags'].split('#')],
-            'link_s': row['Link to image'],
-            'editors_txt': row['Editor(s)'],
-            'translators_txt': row['Translator (optional)']
-        } for row in rows], commitWithin=100)
+    csvreader = csv.DictReader(codecs.iterdecode(response.iter_lines(),
+                               'utf-8'))
+    rows = list(csvreader)
 
-        print(f'Indexed {len(rows):,} records')
+    # check that pgp ids are unique
+    pgpids = [row['PGPID'] for row in rows]
+    if len(pgpids) != len(set(pgpids)):
+        print('Warning: PGPIDs are not unique!')
+
+    # add a quick progress bar here?
+
+    # index pgp data into Solr
+    solr.update.index([{
+        # use PGPID as Solr identifier
+        'id': row['PGPID'],
+        'description_txt': row['Description'],
+        'type_s': row['Type'],
+        'library_s': row['Library'],
+        'shelfmark_s': row['Shelfmark - Current'],
+        'shelfmark_txt': row['Shelfmark - Current'],
+        'tags_txt': [tag.strip() for tag in row['Tags'].split('#')],
+        'tags_ss': [tag.strip() for tag in row['Tags'].split('#')],
+        'link_s': row['Link to image'],
+        'editors_txt': row['Editor(s)'],
+        'translators_txt': row['Translator (optional)']
+    } for row in rows], commitWithin=100)
+
+    print(f'Indexed {len(rows):,} records')
 
 #MR: Library, shelfmark, description, tags, editor(s), translator — tentative list, let’s discuss.
 
