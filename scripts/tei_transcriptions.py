8f94df7bcadebcf28cceee4262629a51e7af127f
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-10-28 15:40:33 -0400

    Revise transcription script to get blocks of text

--- scripts/tei_transcriptions.py
+++ scripts/tei_transcriptions.py
@@ -7,13 +7,28 @@ from eulxml import xmlmap
 from eulxml.xmlmap import teimap
 from flask import current_app
 from flask.cli import with_appcontext
+# from polyglot.detect import Detector
+
+
+class GenizaTeiLine(teimap.TeiLine):
+    name = xmlmap.StringField('local-name(.)')
+    lang = xmlmap.StringField('@xml:lang|tei:span/@xml:lang')
+
+
+class MainText(teimap.TeiDiv):
+    lines = xmlmap.NodeListField('tei:l|tei:label',
+                                 GenizaTeiLine)
 
 
 class GenizaTei(teimap.Tei):
     # extend eulxml TEI to add mappings for the fields we care about
     pgpid = xmlmap.IntegerField('tei:teiHeader//tei:idno[@type="PGP"]')
+    # normally main text content is under text/body/div; but at least one document has no div
+    text = xmlmap.NodeField('tei:text/tei:body/tei:div|tei:text/tei:body[not(tei:div)]', MainText)
     lines = xmlmap.NodeListField('tei:text/tei:body/tei:div/tei:l',
-                                 teimap.TeiLine)
+                                 GenizaTeiLine)
+    labels = xmlmap.NodeListField('tei:text/tei:body/tei:div/tei:label',
+                                  GenizaTeiLine)   # not really a line...
 
 
 @click.command()
@@ -24,10 +39,47 @@ def transcriptions():
     data = {}
 
     for xmlfile in glob.iglob(os.path.join(xml_dir, '*.xml')):
+        # print(os.path.basename(xmlfile))
         print(xmlfile)
 
         tei = xmlmap.load_xmlobject_from_file(xmlfile, GenizaTei)
-        data[tei.pgpid] = [str(l) for l in tei.lines]
+
+        blocks = []
+        label = []
+        lines = []
+        languages = set()
+        for line in tei.text.lines:
+            if line.name == 'label':
+                # append current text block if set, and initialize a new one
+                if lines:
+                    blocks.append({
+                        'label': '\n'.join(label),
+                        'lines': lines,
+                        'languages': list(languages)
+                    })
+                    label = []
+                    lines = []
+
+                # store the label; sometimes there are two in a row
+                label.append(str(line))
+
+            elif line.name == 'l':
+                if line.lang:
+                    # TODO: can we assume a default/unspecified language where not set?
+                    languages.add(line.lang)
+                lines.append(str(line))
+
+        if lines:
+            blocks.append({
+                'label': '\n'.join(label),
+                'lines': lines,
+                'languages': list(languages)
+            })
+
+        docdata = {
+            'blocks': blocks,
+        }
+        data[tei.pgpid] = docdata
 
     with open('data/geniza-transcriptions.json', 'w') as outfile:
         json.dump(data, outfile, indent=4)
