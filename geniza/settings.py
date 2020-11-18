00c5784ed37cf3872cdb64493c715af99aaeb1de
Nick Budak <nbudak@princeton.edu>
2020-11-18 10:03:55 -0500

    Add i18n context processor

--- geniza/settings.py
+++ geniza/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
