2b844c7d4ff1fc28458dab59d409acb1b8dc5daf
Nick Budak <nbudak@princeton.edu>
2020-11-19 11:56:13 -0500

    Update LOCALE_PATHS

--- geniza/settings.py
+++ geniza/settings.py
@@ -124,6 +124,8 @@ LANGUAGES = [
     ("ar", "Arabic"),
 ]
 
+LOCALE_PATHS = [BASE_DIR / "geniza" / "locale"]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
