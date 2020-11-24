6b6ec3b085052ba62a476b6cdbffbccdb569c02f
Nick Budak <nbudak@princeton.edu>
2020-11-24 16:15:36 -0500

    Update path to db file for QA deploys

--- geniza/settings.py
+++ geniza/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "geniza.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "/home/deploy/data/db.sqlite3",
     }
 }
 
