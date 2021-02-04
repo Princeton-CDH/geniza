841d8c2af54713b467101deb595b3f3f2866e35a
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-02-04 17:12:45 -0500

    Load library list by url; configure url in local settings

--- geniza/docs/management/commands/data_exodus.py
+++ geniza/docs/management/commands/data_exodus.py
@@ -1,10 +1,12 @@
+import codecs
 import csv
 
 from django.conf import settings
 from django.contrib.admin.models import ADDITION, LogEntry
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+import requests
 
 from geniza.docs.models import Library
 
@@ -21,21 +23,34 @@ class Command(BaseCommand):
 
         # import list of libraries and abbreviations
         # TODO: csv file location should be configured in local settings
-        with open('pgp_libraries.csv') as librarycsv:
-            csvreader = csv.DictReader(librarycsv)
 
-            # NOTE: because we're using postgres, we can use bulk
-            # create and still get pks for the newly created items
-            libraries = Library.objects.bulk_create([
-                Library(name=row['Library'], abbrev=row['Abbreviation'])
-                for row in csvreader if row['Library'] and row['Abbreviation']
-            ])
+        library_data = self.get_csv('libraries')
+        # create a Library entry for every row in the sheet with both
+        # required values
+        libraries = Library.objects.bulk_create([
+            Library(name=row['Library'], abbrev=row['Abbreviation'])
+            for row in library_data if row['Library'] and row['Abbreviation']
+        ])
+        # NOTE: because we're using postgres, we can use bulk
+        # create and still get pks for the newly created items
 
-            for library in libraries:
-                LogEntry.objects.log_action(
-                    user_id=script_user.id,
-                    content_type_id=library_content_type.pk,
-                    object_id=library.pk,
-                    object_repr=str(library),
-                    change_message=self.logentry_message,
-                    action_flag=ADDITION)
+        # create log entries to document when & how records were created
+        for library in libraries:
+            LogEntry.objects.log_action(
+                user_id=script_user.id,
+                content_type_id=library_content_type.pk,
+                object_id=library.pk,
+                object_repr=str(library),
+                change_message=self.logentry_message,
+                action_flag=ADDITION)
+
+    def get_csv(self, name):
+        # given a name for a file in the configured data import urls,
+        # load the data by url and initialize and return a csv reader
+        response = requests.get(settings.DATA_IMPORT_URLS[name],
+                                stream=True)
+        if response.status_code != requests.codes.ok:
+            raise CommandError('Error accessing CSV for %s: %s' % (name, response))
+
+        return csv.DictReader(codecs.iterdecode(response.iter_lines(),
+                              'utf-8'))
