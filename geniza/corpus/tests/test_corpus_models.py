844d2d8a6d41ae930f459693dfe154d2f97ce983
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-02-17 17:33:39 -0500

    Add natural foreign key to LanguageScript

--- geniza/corpus/tests/test_corpus_models.py
+++ geniza/corpus/tests/test_corpus_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from geniza.corpus.models import Collection, CollectionManager, LanguageScript
+from geniza.corpus.models import Collection, LanguageScript
 
 
 class TestCollection:
@@ -13,17 +13,14 @@ class TestCollection:
         lib = Collection(library='British Library', abbrev='BL')
         assert lib.natural_key() == ('BL',)
 
-
-@pytest.mark.django_db
-class TestCollectionManager:
-
+    @pytest.mark.django_db
     def test_get_by_natural_key(self):
         lib = Collection.objects.create(library='British Library', abbrev='BL')
-
         assert Collection.objects.get_by_natural_key('BL') == lib
 
 
 class TestLanguageScripts:
+
     def test_str(self):
         # test display_name overwrite
         lang = LanguageScript(display_name='Judaeo-Arabic', language='Judaeo-Arabic', script='Hebrew')
@@ -32,3 +29,14 @@ class TestLanguageScripts:
         # test proper string formatting
         lang = LanguageScript(language='Judaeo-Arabic', script='Hebrew')
         assert str(lang) == 'Judaeo-Arabic (Hebrew script)'
+
+    def test_natural_key(self):
+        lang = LanguageScript(language='Judaeo-Arabic', script='Hebrew')
+        assert lang.natural_key() == (lang.language, lang.script)
+
+    @pytest.mark.django_db
+    def test_get_by_natural_key(self):
+        lang = LanguageScript.objects.create(language='Judaeo-Arabic',
+                                             script='Hebrew')
+        assert LanguageScript.objects \
+            .get_by_natural_key(lang.language, lang.script) == lang
