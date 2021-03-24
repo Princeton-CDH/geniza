815d729a9635ae4afdb9b09696ac1e48e225c84c
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-03-24 12:10:33 -0400

    Fix collection default sorting to handle missing fields better

--- geniza/corpus/models.py
+++ geniza/corpus/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models.functions import Concat
 from django.utils.safestring import mark_safe
 from piffle.image import IIIFImageClient
 from piffle.presentation import IIIFPresentation
 from taggit_selectize.managers import TaggableManager
+
 
 from geniza.common.models import TrackChangesModel
 
@@ -32,6 +34,8 @@ class Collection(models.Model):
 
     class Meta:
         ordering = ['lib_abbrev', 'abbrev', 'name', 'library']
+        ordering = [Concat(models.F('lib_abbrev'), models.F('abbrev'),
+                    models.F('name'), models.F('library'))]
         constraints = [
             # require at least one of library OR name
             models.CheckConstraint(
