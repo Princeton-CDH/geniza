00dd309ef4ddd30f9976d49dfd7895b03b8b034f
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-03-30 09:57:16 -0400

    Configure django admin for person

--- geniza/people/admin.py
+++ geniza/people/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from geniza.people.models import Person
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
