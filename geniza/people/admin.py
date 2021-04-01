8d6097f2f085d3ded37c1060160cb143d7bcc0b0
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-04-01 11:09:24 -0400

    Apply tabbed admin translation interface

--- geniza/people/admin.py
+++ geniza/people/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 from geniza.people.models import Person
+from modeltranslation.admin import TabbedTranslationAdmin
 
 @admin.register(Person)
-class PersonAdmin(admin.ModelAdmin):
+class PersonAdmin(TabbedTranslationAdmin, admin.ModelAdmin):
     list_display = ('sort_name',)
     search_fields = ('sort_name',)
