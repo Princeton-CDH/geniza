ac4f3434aa9c2f238c79735810c15772eeb00115
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-04-12 15:55:14 -0400

    #110 Create document detail

--- geniza/corpus/views.py
+++ geniza/corpus/views.py
@@ -8,3 +8,17 @@ class DocumentDetailView(DetailView):
     model = Document
 
     context_object_name = 'document'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        this_document = self.get_object()
+        property_list = ['description', 'doctype',
+            'tag_list', 'all_languages', 'collection', 'shelfmark',
+            'last_modified', 'language_note']
+
+        context['document_dict'] = {
+            attr: getattr(this_document, attr) for attr in property_list if 
+                getattr(this_document, attr)
+        }
+        return context
