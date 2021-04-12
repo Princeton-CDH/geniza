1175bd107204ee59e93f9868e1f1feadbee306e8
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-04-12 17:38:41 -0400

    #110 Restyle template to mimic Gissoo's design

--- geniza/corpus/views.py
+++ geniza/corpus/views.py
@@ -16,16 +16,11 @@ class DocumentDetailView(DetailView):
         return queryset.filter(status=Document.PUBLIC)
 
     def get_context_data(self, **kwargs):
-        '''Find all variables listed in the template'''
         context = super().get_context_data(**kwargs)
-        
-        this_document = self.get_object()
-        property_list = ['description', 'doctype',
-            'tag_list', 'all_languages', 'shelfmark',
-            'last_modified', 'language_note']
 
-        context['document_dict'] = {
-            attr: getattr(this_document, attr) for attr in property_list if 
-                getattr(this_document, attr)
-        }
+        this_document = self.get_object()
+
+        # concatenate all tags
+        context['tags'] = ' '.join(['#' + str(tag) for tag in this_document.tags.all()])
+        
         return context
