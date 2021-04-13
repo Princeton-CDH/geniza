1c629a32ea5c7509e30de6fd15fee2d38f8df615
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-04-13 15:58:36 -0400

    Refactor template and remove unnecessary tag parsing

--- geniza/corpus/tests/test_corpus_views.py
+++ geniza/corpus/tests/test_corpus_views.py
@@ -15,11 +15,3 @@ class TestDocumentDetailView:
         response = client.get(doc.get_absolute_url())
         assert response.status_code == 404
 
-    def test_get_context_data(self, db, client):
-        doc = Document.objects.create()
-
-        # ensure view parses tags correctly
-        doc.tags.add('10th century', 'Medical')
-        response = client.get(doc.get_absolute_url())
-        assert '#10th century' in response.context['tags']
-        assert '#Medical' in response.context['tags']
--- geniza/corpus/views.py
+++ geniza/corpus/views.py
@@ -14,13 +14,3 @@ class DocumentDetailView(DetailView):
         '''Don't show document if it isn't public'''
         queryset = super().get_queryset(*args, **kwargs)
         return queryset.filter(status=Document.PUBLIC)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        this_document = self.get_object()
-
-        # concatenate all tags
-        context['tags'] = ['#' + str(tag) for tag in this_document.tags.all()]
-        
-        return context
