8ee38cf78b46d05d24196b31931ebf9532375047
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-04-27 14:52:22 -0400

    Create preliminary solr schema

---
+++ geniza/common/solr.py
@@ -0,0 +1,14 @@
+from parasolr import schema
+
+
+class SolrSchema(schema.SolrSchema):
+    '''Solr Schema declaration.'''
+
+    # have solr automatically track last index time
+    last_modified = schema.SolrField('pdate', default='NOW')
+
+    # relying on dynamic fields for everything else; see index_data
+    # methods and solr queryset aliases for specifics
+
+    #: copy fields for facets and variant search options
+    copy_fields = {}
