d82f05e41179d6c65c8dab476d6372f6ea825795
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-05-27 16:39:45 -0400

    Update view to iiif url function

--- geniza/corpus/management/commands/import_iiif_urls.py
+++ geniza/corpus/management/commands/import_iiif_urls.py
@@ -35,14 +35,23 @@ class Command(BaseCommand):
         with open(self.csv_path) as f:
             csvreader = csv.DictReader(f)
             for row in csvreader:
-                print(row)
                 self.import_iiif_url(row)
 
     def view_to_iiif_url(self, url):
-        iiif_link = url.replace("/view/", "/iiif/")
-        # view links end with /1 or /2 but iiif link does not include it
-        iiif_link = re.sub(r"/\d$", "", iiif_link)
-        return iiif_link
+        """Get IIIF Manifest URL for a fragment when possible"""
+
+        # cambridge iiif manifest links use the same id as view links
+        # NOTE: should exclude search link like this one:
+        # https://cudl.lib.cam.ac.uk/search?fileID=&keyword=T-s%2013J33.12&page=1&x=0&y=
+        if "cudl.lib.cam.ac.uk/view/" in url:
+            iiif_link = url.replace("/view/", "/iiif/")
+            # view links end with /1 or /2 but iiif link does not include it
+            iiif_link = re.sub(r"/\d$", "", iiif_link)
+            return iiif_link
+
+        # TODO: get new figgy iiif urls for JTS images based on shelfmark
+        # if no url, return empty string for blank instead of null
+        return ""
 
     def import_iiif_url(self, row):
         try:
