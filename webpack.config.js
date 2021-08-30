92faf33c0e0086938f87b665f70fc1b3a73b65a4
Nick Budak <nbudak@princeton.edu>
2021-08-30 15:50:51 -0400

    Disable HMR; add minification and sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path")
 const Autoprefixer = require("autoprefixer")
 const BundleTracker = require("webpack-bundle-tracker")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
 module.exports = (env, options) => ({
     entry: {    // locations and filenames of entry points for bundles
@@ -76,11 +77,23 @@ module.exports = (env, options) => ({
     // https://webpack.js.org/configuration/dev-server/
     devServer: {
         static: path.resolve(__dirname, "sitemedia", "bundles"),
+        hot: false,   // disable hot module reloading since we don't use it
         port: 3000,
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
             'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
         },
-    }
+    },
+    // enable importing .esm.js and other files without specifying extensions
+    resolve: { extensions: [".js", ".esm.js", ".sass", ".scss"], },
+    // generate source maps for easier debugging
+    devtool: "source-map",
+    // minify JS and CSS in production
+    optimization: {
+        minimizer: [
+            "...",  // shorthand; minify JS using the default TerserPlugin
+            new CssMinimizerPlugin()    // also minify CSS
+        ]
+    },
 })
