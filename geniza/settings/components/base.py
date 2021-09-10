287fb6c6b8617a091ab7856f5377a6ce9090919a
Nick Budak <nbudak@princeton.edu>
2021-09-10 15:58:34 -0400

    Add spacing harmonic scale and use across styles

--- geniza/settings/components/base.py
+++ geniza/settings/components/base.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "csp.middleware.CSPMiddleware",
+    # "csp.middleware.CSPMiddleware",
 ]
 
 ROOT_URLCONF = "geniza.urls"
