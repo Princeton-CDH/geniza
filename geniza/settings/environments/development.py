4fc6ccba4f539f6d641862843b2b07da6c9cc138
Nick Budak <nbudak@princeton.edu>
2021-09-10 11:59:47 -0400

    Allow DJDT thru CSP in development

--- geniza/settings/environments/development.py
+++ geniza/settings/environments/development.py
@@ -1,3 +1,4 @@
-# if in debug mode, allow webpack dev server through CSP controls
+# if in debug mode, allow webpack dev server and django debug toolbar thru CSP
 if DEBUG:
+    CSP_SCRIPT_SRC = ("http://localhost:8000",)
     CSP_CONNECT_SRC = ("ws://localhost:3000",)
