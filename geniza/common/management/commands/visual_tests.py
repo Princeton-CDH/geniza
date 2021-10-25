2c86694e9c4c8585057161912b479247435a9477
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-10-25 13:07:34 -0400

    Adapt visual testing manage command from cdhweb

---
+++ geniza/common/management/commands/visual_tests.py
@@ -0,0 +1,58 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from percy import percy_snapshot
+from selenium import webdriver
+
+
+class Command(BaseCommand):
+    """Execute visual regression tests against a running django server."""
+
+    help = __doc__
+
+    def get_browser(self):
+        """Initialize a browser driver to use for taking snapshots."""
+        options = webdriver.ChromeOptions()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--headless")
+        return webdriver.Chrome(options=options)
+
+    def take_snapshots(self, browser):
+        """Take DOM snapshots of a set of URLs and upload to Percy."""
+
+        # homepage TODO
+        # browser.get("http://localhost:8000/")
+        # percy_snapshot(browser, "Home")
+
+        # content page TODO
+        # browser.get("http://localhost:8000/content")
+        # percy_snapshot(browser, "Content Page")
+
+        # document search
+        # search term matches description of 3951
+        browser.get("http://localhost:8000/documents?q=Tujib")
+        percy_snapshot(browser, "Document Search")
+
+        # document detail
+        browser.get("http://localhost:8000/documents/3951/")
+        percy_snapshot(browser, "Document Details")
+
+        # document scholarhip
+        browser.get("http://localhost:8000/documents/3951/")
+        percy_snapshot(browser, "Document Scholarship Records")
+
+        # 404 page TODO
+        # browser.get("http://localhost:8000/bad-url")
+        # percy_snapshot(browser, "404 Page")
+
+        # 500 page TODO
+        # browser.get("http://localhost:8000/500")
+        # percy_snapshot(browser, "500 Page")
+
+    def handle(self, *args, **options):
+        # spin up browser and take snapshots; shut down when finished
+        browser = self.get_browser()
+        self.take_snapshots(browser)
+        browser.quit()
