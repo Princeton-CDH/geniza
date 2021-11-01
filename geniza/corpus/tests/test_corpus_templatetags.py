85f9ee93d4d672acc743615f69f5abd1e6a9fc91
Ben Silverman <ben@performantsoftware.com>
2021-11-01 11:40:54 -0400

    #307 - Add tests for "alphabetize" template tag

---
+++ geniza/corpus/tests/test_corpus_templatetags.py
@@ -0,0 +1,34 @@
+import pytest
+
+from geniza.corpus.templatetags import corpus_extras
+
+
+class TestCorpusExtrasTemplateTags:
+    def test_alphabetize(self):
+        """Should lowercase and alphabetize a list of strings"""
+        lst = ["Test", "hello", "abc", "Def"]
+        alphabetized = corpus_extras.alphabetize(lst)
+        assert alphabetized[0] == "abc"
+        assert alphabetized[1] == "def"
+        assert alphabetized[2] == "hello"
+        assert alphabetized[3] == "test"
+
+    def test_alphabetize_bad_list(self):
+        """Should throw TypeError when list contains non-strings"""
+        with pytest.raises(TypeError) as err:
+            bad_list = [1, 2, 3, "hi", ["test"]]
+            corpus_extras.alphabetize(bad_list)
+        assert "Argument must be a list of strings" in str(err)
+
+    def test_alphabetize_not_list(self):
+        """Should throw TypeError when argument is not a list"""
+        with pytest.raises(TypeError) as err:
+            not_list = -4
+            corpus_extras.alphabetize(not_list)
+        assert "Argument must be a list of strings" in str(err)
+
+    def test_alphabetize_empty_list(self):
+        """Should process empty list without raising exception"""
+        lst = []
+        alphabetized = corpus_extras.alphabetize(lst)
+        assert alphabetized == []
