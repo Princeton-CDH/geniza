198b2706acc0455a7dc29c3c032bb490b1bba3d4
Ben Silverman <ben@performantsoftware.com>
2021-11-04 15:35:45 -0400

    #308 - Revert colon to semicolon (until approved)

--- geniza/corpus/models.py
+++ geniza/corpus/models.py
@@ -496,7 +496,7 @@ class Document(ModelIndexable):
     @property
     def title(self):
         """Short title for identifying the document, e.g. via search."""
-        return f"{self.doctype or _('Unknown type')}: {self.shelfmark_display or '??'}"
+        return f"{self.doctype or _('Unknown type')}; {self.shelfmark_display or '??'}"
 
     def editions(self):
         """All footnotes for this document where the document relation includes
