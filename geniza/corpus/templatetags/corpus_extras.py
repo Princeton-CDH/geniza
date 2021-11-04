1ee8208d49af5616fcc97329a8c48c6c9b5adc64
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-11-04 12:55:59 -0400

    Port dict_item template tag from ppa

--- geniza/corpus/templatetags/corpus_extras.py
+++ geniza/corpus/templatetags/corpus_extras.py
@@ -10,3 +10,16 @@ def alphabetize(value):
         return sorted([s.lower() for s in value])
     else:
         raise TypeError("Argument must be a list of strings")
+
+
+# dict_item filter ported from ppa codebase
+
+
+@register.filter
+def dict_item(dictionary, key):
+    """'Template filter to allow accessing dictionary value by variable key.
+    Example use::
+
+        {{ mydict|dict_item:keyvar }}
+    """
+    return dictionary.get(key, None)
