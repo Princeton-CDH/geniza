b7d846cc9b9e4608a059fbb10bd4eaf3ba71dabf
Ben Silverman <ben@performantsoftware.com>
2021-11-29 13:42:25 -0500

    #312 - Improve CSS animations for nav menus

--- sitemedia/js/submenu.js
+++ sitemedia/js/submenu.js
@@ -1,3 +1,27 @@
+// Better menu animation when JS enabled
+
+function closeAboutMenu() {
+    aboutMenu = document.querySelector("ul#about-menu");
+    if (aboutMenu && !aboutMenu.classList.contains("slideout-right-mobile")) {
+        aboutMenu.classList.add("slideout-right-mobile");
+    }
+    return false;
+}
+function closeMainMenu(mainMenu) {
+    if (mainMenu && !mainMenu.classList.contains("slideout-left-mobile")) {
+        mainMenu.classList.add("slideout-left-mobile");
+    }
+    return false;
+}
+
+let mainMenu = document.querySelector("ul#menu");
+mainMenu
+    .querySelector("a.home-link")
+    .addEventListener("click", () => closeMainMenu(mainMenu));
+mainMenu
+    .querySelector("a#close-main-menu")
+    .addEventListener("click", () => closeMainMenu(mainMenu));
+
 // Adapted from W3C web accessibility tutorials:
 // https://www.w3.org/WAI/tutorials/menus/flyout/#flyoutnavkbfixed
 
@@ -11,6 +35,18 @@ Array.prototype.forEach.call(menuItems, function (el) {
             this.parentNode.className = "menu-item has-submenu";
             this.setAttribute("aria-expanded", "false");
         }
+        // Animate main menu closing on opening submenu
+        mainMenu = this.parentNode.parentNode;
+        closeMainMenu(mainMenu);
         return false;
     });
+    // Animate submenu closing on clicking relevant buttons
+    el.querySelector("a#back-to-main-menu").addEventListener(
+        "click",
+        closeAboutMenu
+    );
+    el.querySelector("a#close-about-menu").addEventListener(
+        "click",
+        closeAboutMenu
+    );
 });
