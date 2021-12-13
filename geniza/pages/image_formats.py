c8d217997bcb284be16daa7b5ec69ed02d4a1f13
Ben Silverman <ben@performantsoftware.com>
2021-12-13 10:27:50 -0500

    #328 - Comments explaining image_formats.py

--- geniza/pages/image_formats.py
+++ geniza/pages/image_formats.py
@@ -7,8 +7,10 @@ from wagtail.images.formats import (
 
 
 class CaptionedImageFormat(Format):
-    def image_to_html(self, image, alt_text, extra_attributes=None):
+    """Custom image format for including a caption"""
 
+    def image_to_html(self, image, alt_text, extra_attributes=None):
+        """Wraps supplied image in figure tags, and supplied alt text in figcaption tags."""
         default_html = super().image_to_html(image, alt_text, extra_attributes)
 
         return format_html(
@@ -16,9 +18,12 @@ class CaptionedImageFormat(Format):
         )
 
 
+# Unregister Wagtail default image formats
 unregister_image_format("fullwidth")
 unregister_image_format("left")
 unregister_image_format("right")
+
+# Register our custom image format
 register_image_format(
     CaptionedImageFormat(
         "captioned_fullwidth", "Full width captioned", "bodytext-image", "width-800"
