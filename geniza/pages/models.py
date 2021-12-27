105a14a3f99d0b49cd740fb0b07182a72da326e2
Ben Silverman <ben@performantsoftware.com>
2021-12-27 09:57:52 -0500

    #465 - Rm Contributor, CreditsPage models & logic

--- geniza/pages/models.py
+++ geniza/pages/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from modeltranslation.manager import MultilingualManager
 from wagtail.admin.edit_handlers import FieldPanel, RichTextFieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
@@ -30,7 +29,7 @@ class HomePage(Page):
     )
     # can only be child of Root
     parent_page_types = [Page]
-    subpage_types = ["pages.ContentPage", "pages.CreditsPage"]
+    subpage_types = ["pages.ContentPage"]
     content_panels = Page.content_panels + [
         FieldPanel("description"),
         RichTextFieldPanel("body"),
@@ -73,44 +72,3 @@ class ContentPage(Page):
         context = super(ContentPage, self).get_context(request)
         context["page_type"] = "content-page"
         return context
-
-
-class ContributorManager(MultilingualManager):
-    def get_by_natural_key(self, last_name, first_name):
-        return self.get(last_name=last_name, first_name=first_name)
-
-
-class Contributor(models.Model):
-    """Contributor to be listed on the credits page"""
-
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    role = models.CharField(max_length=255)
-
-    objects = ContributorManager()
-
-    class Meta:
-        ordering = ["last_name", "first_name"]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["first_name", "last_name"], name="contributor_unique_name"
-            )
-        ]
-
-    def __str__(self):
-        """Creator full name, with first name first"""
-        return " ".join([n for n in [self.first_name, self.last_name] if n])
-
-    def natural_key(self):
-        return (self.last_name, self.first_name)
-
-
-class CreditsPage(ContentPage):
-    """:class:`ContentPage` model for displaying a credits page."""
-
-    content_panels = Page.content_panels + [
-        FieldPanel("description"),
-    ]
-
-    def contributors(self):
-        return Contributor.objects.all()
