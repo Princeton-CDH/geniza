53a943bfec9f7729496229b4a5577265a9a10dae
Ben Silverman <ben@performantsoftware.com>
2021-12-27 13:18:16 -0500

    #410 - Mv .localized call from ctxproc to template

--- geniza/context_processors.py
+++ geniza/context_processors.py
@@ -13,9 +13,6 @@ def template_globals(request):
         "SHOW_TEST_WARNING": getattr(settings, "SHOW_TEST_WARNING", False),
         "FONT_URL_PREFIX": getattr(settings, "FONT_URL_PREFIX", ""),
         "site": site,
-        "about_page": site.root_page.get_children()
-        .exact_type(AboutPage)
-        .first()
-        .localized,
+        "about_page": site.root_page.get_children().exact_type(AboutPage).first(),
     }
     return context_extras
