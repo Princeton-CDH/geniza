c40742b64a19f640fd72544b2fefb3ce5682aae3
Kevin McElwee <kevinrmcelwee@gmail.com>
2022-01-05 16:48:51 -0500

    [WIP: failing test] Update DocumentScholarship for solr

--- geniza/corpus/tests/test_sitemaps.py
+++ geniza/corpus/tests/test_sitemaps.py
@@ -10,6 +10,7 @@ from geniza.corpus.sitemaps import (
     DocumentSitemap,
     solr_timestamp_to_date,
 )
+from geniza.corpus.tests.conftest import make_document
 from geniza.footnotes.models import Footnote
 
 
@@ -44,26 +45,30 @@ class TestDocumentSitemap:
 
 
 class TestDocumentScholarshipSitemap:
-    def test_items(self, document, source):
-        # Ensure that only documents with footnotes are returned
-        assert document.footnotes.count() == 0
-        sitemap = DocumentScholarshipSitemap()
-        assert len(sitemap.items()) == 0
-
-        footnote = Footnote.objects.create(
+    def test_items(self, document, source, suppressed_document, fragment):
+        document_with_footnote = make_document(fragment, id=41)
+        Footnote.objects.create(
             source=source,
-            content_object=document,
+            content_object=document_with_footnote,
             location="p.1",
             doc_relation=Footnote.EDITION,
         )
-        sitemap = DocumentScholarshipSitemap()
-        assert document in sitemap.items()
 
-        # Ensure that documents are supressed if they aren't public
-        document.status = Document.SUPPRESSED
-        document.save()
+        SolrClient().update.index(
+            [
+                document.index_data(),  # no scholarship records
+                suppressed_document.index_data(),  # suppressed
+                document_with_footnote.index_data(),  # has scholarship
+            ],
+            commit=True,
+        )
+
+        # Ensure that only public documents with footnotes are returned
         sitemap = DocumentScholarshipSitemap()
-        assert len(sitemap.items()) == 0
+        assert document_with_footnote.id in [obj["pgpid"] for obj in sitemap.items()]
+        assert suppressed_document.id not in [obj["pgpid"] for obj in sitemap.items()]
+        # TEST FAILURE: How do you filter by footnotes__isnull for a solr queryset?
+        assert document.id not in [obj["pgpid"] for obj in sitemap.items()]
 
     def test_location(self, document, source):
         footnote = Footnote.objects.create(
