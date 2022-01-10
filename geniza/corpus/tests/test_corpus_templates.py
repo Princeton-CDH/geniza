62ee8238881d5af0cddc5ef7588fe577d4bf123b
Ben Silverman <ben@performantsoftware.com>
2022-01-10 17:01:12 -0500

    #398 - Revert disabled external links test

--- geniza/corpus/tests/test_corpus_templates.py
+++ geniza/corpus/tests/test_corpus_templates.py
@@ -265,14 +265,10 @@ class TestDocumentTabsSnippet:
         fragment.save()
         response = client.get(document.get_absolute_url())
 
-        underline_div = (
-            "<div class='tab-underline' tabindex='-1' aria-hidden='true'></div>"
-        )
-
         # disabled (not yet implemented) for MVP
         assertContains(
             response,
-            "<li class='disabled'><span>External Links</span>%s</li>" % underline_div,
+            "<li class='disabled'><span>External Links</span></li>",
             html=True,
         )
 
