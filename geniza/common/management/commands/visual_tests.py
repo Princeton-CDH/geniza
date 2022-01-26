e630d6be6eae077d3104c14912dc4186e67c75c7
Ben Silverman <ben@performantsoftware.com>
2022-01-26 15:17:31 -0500

    #401 - Fix percy issues
    - Add Cambridge to allowed hostnames
    - Update all screenshots to use PGPID 2811
    - [run percy]

--- geniza/common/management/commands/visual_tests.py
+++ geniza/common/management/commands/visual_tests.py
@@ -72,7 +72,7 @@ class Command(BaseCommand):
         )
 
         # about submenu open on both desktop and mobile
-        browser.get("http://localhost:8000/en/documents/2532/#menu")
+        browser.get("http://localhost:8000/en/documents/2811/#menu")
         # open about menu
         browser.find_element_by_id("open-about-menu").send_keys(Keys.ENTER)
         # custom CSS to ensure that on mobile, the about menu transition is disabled and the menu
