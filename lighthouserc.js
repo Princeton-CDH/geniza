6c7e2143327716d7a0aa59d157b54b4f11ddeb1d
Ben Silverman <ben@performantsoftware.com>
2022-02-17 16:36:45 -0500

    Allow 1 unminified JS per page in lighthosue
    ref #550

--- lighthouserc.js
+++ lighthouserc.js
@@ -43,6 +43,8 @@ module.exports = {
                 // next two are only failing because of OpenSeaDragon, so disable for now
                 "unsized-images": "off",
                 "unused-javascript": "off",
+                // allow 1 unminified javascript per page
+                "unminified-javascript": ["error", { maxLength: 1 }],
             },
         },
     },
