00939f1055cc2e9c41003ba527a9ff24d80df34c
Ben Silverman <ben@performantsoftware.com>
2022-02-18 13:01:19 -0500

    Ensure sort choice is not overwritten on page load
    ref #550

--- sitemedia/js/controllers/search_controller.js
+++ sitemedia/js/controllers/search_controller.js
@@ -1,4 +1,5 @@
 // src/controllers/search.js
+
 import { Controller } from "@hotwired/stimulus";
 import { ApplicationController, useDebounce } from "stimulus-use";
 
@@ -7,44 +8,45 @@ export default class extends Controller {
     static debounces = ["submit"];
 
     connect() {
-        this.defaultSort = "scholarship_desc";
         useDebounce(this);
     }
 
     submit() {
-        console.log("submitting the form");
         this.element.submit();
     }
 
-    sortTargetConnected(element) {
+    sortTargetConnected() {
         // when sort targets are first connected,
         // check and disable relevance sort if appropriate
+        this.relevanceSortElement = this.sortTargets.find(
+            (target) => target.value === "relevance"
+        );
+        this.defaultSortElement = this.sortTargets.find(
+            (target) => target.value === "scholarship_desc"
+        );
         this.updateSort();
     }
 
-    updateSort() {
-        console.log("updateSort");
+    updateSort(event) {
         // when query is empty, disable sort by relevance
         if (this.queryTarget.value.trim() == "") {
             this.disableRelevanceSort();
-        } else {
+            // if this was triggered by an event and not in sortTargetConnected, sort by relevance
+        } else if (event) {
             this.sortByRelevance();
         }
     }
 
     sortByRelevance() {
-        this.sortTargets.forEach((radio) => {
-            // enable and check relevance sort
-            if (radio.value === "relevance") {
-                radio.checked = true;
-                radio.disabled = false;
-                radio.ariaDisabled = false;
-                this.setSortLabel(radio.parentElement.textContent);
-            } // disable all other buttons
-            else {
+        this.relevanceSortElement.checked = true;
+        this.relevanceSortElement.disabled = false;
+        this.relevanceSortElement.ariaDisabled = false;
+        this.setSortLabel(this.relevanceSortElement.parentElement.textContent);
+        this.sortTargets
+            .filter((el) => el.value !== "relevance")
+            .forEach((radio) => {
                 radio.checked = false;
-            }
-        });
+            });
     }
 
     setSortLabel(label) {
@@ -52,21 +54,16 @@ export default class extends Controller {
     }
 
     disableRelevanceSort() {
-        this.sortTargets.forEach((radio) => {
-            // uncheck and disable relevance sort option
-            if (radio.value === "relevance") {
-                radio.checked = false;
-                radio.disabled = true;
-                radio.ariaDisabled = true;
-            }
-            // if relevance sort was checked, set back to default
-            else if (
-                // this.relevanceSortElement.checked &&
-                radio.value === this.defaultSort
-            ) {
-                radio.checked = true;
-                this.setSortLabel(radio.parentElement.textContent);
-            }
-        });
+        // if relevance sort was checked, set back to default
+        if (this.relevanceSortElement.checked) {
+            this.relevanceSortElement.checked = false;
+            this.defaultSortElement.checked = true;
+            this.setSortLabel(
+                this.defaultSortElement.parentElement.textContent
+            );
+        }
+        // disable relevance sort
+        this.relevanceSortElement.disabled = true;
+        this.relevanceSortElement.ariaDisabled = true;
     }
 }
