1ea6f08a688a031d2e004045a35e011694a6c184
Ben Silverman <ben@performantsoftware.com>
2022-02-21 11:47:15 -0500

    Disable turbo import until implemented fully ref #550

--- sitemedia/js/stimulus_app.js
+++ sitemedia/js/stimulus_app.js
@@ -1,6 +1,7 @@
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
-import * as Turbo from "@hotwired/turbo";
+// TODO: Implement turbo
+// import * as Turbo from "@hotwired/turbo";
 
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
