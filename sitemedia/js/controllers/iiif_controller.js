3555214d4d4f8c29e605b4d5a67c11dd6e6ca135
Ben Silverman <ben@performantsoftware.com>
2022-03-01 16:14:48 -0500

    Ensure OSD removed on disconnect (#634)

--- sitemedia/js/controllers/iiif_controller.js
+++ sitemedia/js/controllers/iiif_controller.js
@@ -4,21 +4,29 @@ import { Controller } from "@hotwired/stimulus";
 import OpenSeadragon from "openseadragon";
 
 export default class extends Controller {
-    connect() {
-        let target = document.getElementById("iiif-images");
-        if (target != null) {
-            let viewer = OpenSeadragon({
-                id: target.id,
-                prefixUrl:
-                    "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/3.0.0/images/",
-                tileSources: JSON.parse(target.dataset.iiifUrls),
-                sequenceMode: true,
-                preserveViewport: true,
-                autoHideControls: false,
-                showHomeControl: false,
-                showRotationControl: true,
-                crossOriginPolicy: "Anonymous",
-            });
+    static targets = ["iiifContainer"];
+
+    iiifContainerTargetConnected() {
+        // inject OSD into the iiif container
+        OpenSeadragon({
+            id: this.iiifContainerTarget.id,
+            prefixUrl:
+                "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/3.0.0/images/",
+            tileSources: JSON.parse(this.iiifContainerTarget.dataset.iiifUrls),
+            sequenceMode: true,
+            preserveViewport: true,
+            autoHideControls: false,
+            showHomeControl: false,
+            showRotationControl: true,
+            crossOriginPolicy: "Anonymous",
+        });
+    }
+    iiifContainerTargetDisconnected() {
+        // ensure OSD is removed on disconnect by removing all child nodes from target
+        while (this.iiifContainerTarget.firstChild) {
+            this.iiifContainerTarget.removeChild(
+                this.iiifContainerTarget.firstChild
+            );
         }
     }
 }
