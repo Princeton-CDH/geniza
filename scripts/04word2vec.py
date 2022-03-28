4f3b72b464375e1c5f494cc19032bce9b20aa0ae
Vineet Bansal <vineetbansal@protonmail.com>
2022-03-28 10:48:53 -0400

    word2vec script and supplementary stuff

---
+++ scripts/04word2vec.py
@@ -0,0 +1,234 @@
+import sys
+from collections import Counter
+import textwrap
+from pprint import pprint
+import os
+import shutil
+import logging
+import pickle
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+import nltk
+import pyLDAvis
+import pyLDAvis.gensim_models
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel, Word2Vec
+import spacy
+from gensim.models.ldamodel import LdaModel
+from nltk.collocations import BigramCollocationFinder, BigramAssocMeasures
+
+from ldamallet import LdaMallet
+
+from sklearn.cluster import AffinityPropagation
+from sklearn import metrics
+
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn.decomposition import PCA
+
+logger = logging.getLogger(__name__)
+
+
+def process_words(texts, stop_words, allowed_tags=None, allowed_ners=None, min_len=3, max_len=30):
+    # python -m spacy download en_core_web_sm
+    # English pipeline optimized for CPU. Components: tok2vec, tagger, parser, senter, ner, attribute_ruler, lemmatizer.
+    # Other pipelines at https://spacy.io/models/en
+    nlp = spacy.load('en_core_web_sm')
+
+    # simple_preprocess => Convert a document into a list of lowercase tokens, ignoring tokens that are too short or too long
+    texts_ = []
+    for i, doc in enumerate(texts):
+        text = [word for word in simple_preprocess(str(doc), deacc=True, min_len=min_len, max_len=max_len) if
+             word not in stop_words]
+        texts_.append(text)
+    texts = texts_
+
+    texts_out = []
+    # implement lemmatization and filter out unwanted part of speech tags
+    for i, sent in enumerate(texts):
+        doc = nlp(' '.join(sent))
+        text_out = []
+
+        if allowed_tags == ['NOUN']:
+            for ent in doc.ents:
+                if (not allowed_ners) or (ent.label_ in allowed_ners):
+                    text_out.append(ent.text)
+        else:
+            for token in doc:
+                if (not allowed_tags) or (token.pos_ in allowed_tags):
+                    text_out.append(token.lemma_)
+
+        texts_out.append(text_out)
+    return texts_out
+
+
+def plot_word2vec_model(model):
+    words = list(model.wv.key_to_index)
+    X = model.wv[words]
+    pca = PCA(n_components=2)
+    result = pca.fit_transform(X)
+    plt.scatter(result[:, 0], result[:, 1])
+    for i, word in enumerate(words):
+        plt.annotate(word, xy=(result[i, 0], result[i, 1]))
+
+
+def vectorize(list_of_docs, model):
+    features = np.zeros((len(list_of_docs), model.vector_size))
+    for i, tokens in enumerate(list_of_docs):
+        vectors = [model.wv[token] for token in tokens if token in model.wv]
+        if vectors:
+            features[i, :] = np.mean(vectors, axis=0)
+
+    return features
+
+
+PARAMS = dict(
+    MAX_DOCS=None,                    # for quick code testing - int or None (all docs)
+    MIN_LEN=3,                        # words less than this length will be filtered
+    MAX_LEN=100,                      # words more than this length will be filtered
+    ALLOWED_TAGS=[                    # POS tags to consider; None or empty to consider all
+                                      # See https://github.com/explosion/spaCy/blob/b7ba7f78a28ef71fca60415d0165e27a058d1946/spacy/glossary.py#L17
+    ],
+    ALLOWED_NERS=[                    # Named-entities to consider; None or empty to consider all
+                                      # Only applicable when ALLOWED_TAGS = ['NOUN'] (only)
+                                      # See https://github.com/ explosion/spaCy/blob/b7ba7f78a28ef71fca60415d0165e27a058d1946/spacy/glossary.py#L318
+                                      # e.g. muslim=NORP, islam=ORG, damascus=GPE
+    ],
+    BIGRAM=False,                     # Form bigrams before creating corpus?
+    BIGRAM_MIN_PMI=5,                 # Min. PMI in order to create bigrams (determine by manual inspection of generated bigrams.txt)
+    BIGRAM_MIN_FREQ=20,               # Min. freq of co-occurring tokens before they can be considered a bigram
+
+    COMMON_WORDS_MAX_FREQUENCY=10000,  # For root words, the max. frequency beyond which they're not useful
+    COMMON_WORDS_MAX_DOCS=0.5,        # For root words, max docs (absolute or relative) beyond which they're not useful
+    COMMON_WORDS_MIN_DOCS=5,          # For root words, min docs (absolute or relative) beyond which they're not useful
+
+    KEEP_TOKENS=[],                   # Root words to preserve in the vocabulary regardless of their frequency (high or low)
+
+    # No. of topics - numeric or a range
+    K=range(10, 50),
+
+    WORD2VEC_VECTOR_SIZE=200,
+    WORD2VEC_WINDOW=10,
+    WORD2VEC_EPOCHS=10,
+
+    AFFINITY_N_DOCS=None,
+)
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO'))
+    sns.set()
+
+    # ----------- Copy script and params -------------- #
+    existing_runs = sorted([d for d in os.listdir('results') if os.path.isdir(f'results/{d}')])
+    if existing_runs:
+        run_id = int(existing_runs[-1]) + 1
+    else:
+        run_id = 1
+    run_id = f'{run_id:04}'
+
+    output_dir = os.path.join('results', run_id)
+    os.makedirs(output_dir)
+    logger.addHandler(logging.FileHandler(os.path.join(output_dir, 'log.txt')))
+    shutil.copy(__file__, output_dir)
+    with open(os.path.join(output_dir, 'params.txt'), 'w') as f:
+        pprint(PARAMS, f)
+
+    _stop_words = []
+    stopwords_file = 'stopwords.txt'
+    if os.path.exists(stopwords_file):
+        _stop_words = open(stopwords_file, 'r').read().splitlines()
+        shutil.copy(stopwords_file, os.path.join(output_dir, stopwords_file))
+    # ----------- Copy script and params -------------- #
+
+    stop_words = _stop_words + nltk.corpus.stopwords.words('english')  # nltk.download('stopwords')
+
+    df = pd.read_csv('data.csv', dtype={'tags': str})[:PARAMS['MAX_DOCS']]
+    logger.info(f'No. of records = {len(df)}')
+    df = df.dropna(subset=['description'])
+    logger.info(f'After dropping records with missing description, no. of records = {len(df)}')
+    df['tags'] = df['tags'].str.lower()
+
+    data = list(df.description)
+    data = process_words(data, stop_words=stop_words, allowed_tags=PARAMS['ALLOWED_TAGS'],
+                         allowed_ners=PARAMS['ALLOWED_NERS'], min_len=PARAMS['MIN_LEN'], max_len=PARAMS['MAX_LEN'])
+    logger.info(f'After filtering stopwords/short words/lemmatization, no. of records = {len(data)}')
+
+    if not os.path.exists(os.path.join(output_dir, 'wvmodel.bin')):
+        model = Word2Vec(
+            sentences=data,
+            vector_size=PARAMS['WORD2VEC_VECTOR_SIZE'],
+            workers=8,
+            sg=1,
+            window=PARAMS['WORD2VEC_WINDOW'],
+            epochs=PARAMS['WORD2VEC_EPOCHS'],
+            seed=12345
+        )
+        model.save(os.path.join(output_dir, 'wvmodel.bin'))
+        with open(os.path.join(output_dir, 'wvmodel_keys.txt'), 'w') as f:
+            f.write('\n'.join(model.wv.index_to_key))
+    else:
+        model = Word2Vec.load('wvmodel.bin')
+
+    wv = model.wv
+    # # wv = api.load('word2vec-google-news-300')  # pre-trained model
+    # plot_word2vec_model(model)
+
+    print(wv.most_similar('islam', topn=20))
+
+    n_docs = PARAMS['AFFINITY_N_DOCS']
+    X = vectorize(data[:n_docs], model=model)
+    logger.info(f'Vectorization of {n_docs} documents done.')
+
+    logger.info('Fitting AffinityPropagation model to documents..')
+    af = AffinityPropagation(verbose=True, random_state=43423, damping=0.5).fit(X)
+    cluster_centers_indices = af.cluster_centers_indices_
+    labels = af.labels_
+
+    with open(os.path.join(output_dir, 'affinity_clustering.html'), 'w') as f:
+        for label in np.unique(labels):
+            f.write(f'<hr/><b>Cluster {label}</b><hr/>')
+            docs = np.where(labels == label)[0]
+            docs_mean_vector = X[docs].mean(axis=0)
+            terms = ', '.join([term for (term, _) in wv.most_similar(docs_mean_vector)])
+            f.write(f'<i>{terms}</i><hr/>')
+            for doc in docs:
+                row = df.iloc[doc]
+                f.write(f'<a target="_blank" href="{row.url}">{row.pgpid}</a><br/>')
+                f.write(str(row.description) + '<br/>')
+
+    n_clusters_ = len(cluster_centers_indices)
+    logger.info(f'Estimated number of clusters: {n_clusters_}')
+
+    coeff = metrics.silhouette_score(X, labels, metric='sqeuclidean')
+    logger.info(f'Silhouette Coefficient: {coeff}')
+
+    plt.close('all')
+    plt.figure(1)
+    plt.clf()
+
+    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+    for k, col in zip(range(n_clusters_), colors):
+        class_members = labels == k
+        cluster_center = X[cluster_centers_indices[k]]
+        plt.plot(X[class_members, 0], X[class_members, 1], col + '.')
+        plt.plot(
+            cluster_center[0],
+            cluster_center[1],
+            'o',
+            markerfacecolor=col,
+            markeredgecolor='k',
+            markersize=14,
+        )
+        for x in X[class_members]:
+            plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+
+    plt.title(f'Estimated number of clusters: {n_clusters_}')
+    plt.savefig(os.path.join(output_dir, 'clusters.png'))
---
+++ scripts/results/README.md
@@ -0,0 +1,4 @@
+This folder contains folders with names of the form `nnnn`, one for each run
+of a python script one level above, with a copy of the script itself, the 
+parameters used for the script, logging output, and any artifacts that the
+script generates.
