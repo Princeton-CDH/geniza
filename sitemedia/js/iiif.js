a6dadd48ae0b510fa8cce5bd572ee726acfb5b7f
Ben Silverman <ben@performantsoftware.com>
2022-04-14 16:29:07 -0400

    Remove extraneous turbo import from iiif.js (#724)

--- sitemedia/js/iiif.js
+++ sitemedia/js/iiif.js
@@ -1,6 +1,5 @@
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
-import * as Turbo from "@hotwired/turbo";
 
 const application = Application.start();
 // only require iiif_controller.js
