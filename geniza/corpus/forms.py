b0a8735ff1d4961420b0d8fbd28ae8b2c73caad3
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-06-02 16:35:09 -0400

    Adjust sort option order and input date labels

--- geniza/corpus/forms.py
+++ geniza/corpus/forms.py
@@ -164,19 +164,19 @@ class DocumentSearchForm(RangeForm):
         # Translators: label for sort in random order
         ("random", _("Random")),
         # Translators: label for sort by document date (most recent first)
-        ("docdate_desc", _("Document Date (Latest-Earliest)")),
+        ("docdate_desc", _("Document Date (Latest–Earliest)")),
         # Translators: label for sort by document date (oldest first)
-        ("docdate_asc", _("Document Date (Earliest-Latest)")),
-        # Translators: label for sort by document input date (most recent first)
-        ("input_date_desc", _("Input Date (Latest-Earliest)")),
-        # Translators: label for sort by document input date (oldest first)
-        ("input_date_asc", _("Input Date (Earliest-Latest)")),
+        ("docdate_asc", _("Document Date (Earliest–Latest)")),
+        # Translators: label for alphabetical sort by shelfmark
+        ("shelfmark", _("Shelfmark (A–Z)")),
         # Translators: label for descending sort by number of scholarship records
         ("scholarship_desc", _("Scholarship Records (Most–Least)")),
         # Translators: label for ascending sort by number of scholarship records
         ("scholarship_asc", _("Scholarship Records (Least–Most)")),
-        # Translators: label for alphabetical sort by shelfmark
-        ("shelfmark", _("Shelfmark (A-Z)")),
+        # Translators: label for sort by when document was added to PGP (most recent first)
+        ("input_date_desc", _("PGP Input Date (Latest–Earliest)")),
+        # Translators: label for sort by when document was added to PGP (oldest first)
+        ("input_date_asc", _("PGP Input Date (Earliest–Latest)")),
     ]
     # Translators: label for start year when filtering by date range
     _("From year")
