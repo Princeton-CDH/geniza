1250c7c29130a0d3e2dc7601acd220fd7d2ced2a
Ben Silverman <ben@performantsoftware.com>
2022-06-07 16:49:52 -0400

    Implement deep zoom switching (#891)

--- sitemedia/js/controllers/iiif_controller.js
+++ sitemedia/js/controllers/iiif_controller.js
@@ -4,32 +4,49 @@ import { Controller } from "@hotwired/stimulus";
 import OpenSeadragon from "openseadragon";
 
 export default class extends Controller {
-    static targets = ["iiifContainer"];
+    static targets = ["imageContainer"];
 
-    iiifContainerTargetConnected() {
-        // inject OSD into the iiif container
+    imageContainerTargetDisconnected(element) {
+        // remove OSD on target disconnect (i.e. leaving page)
+        this.removeOpenSeaDragon(element);
+    }
+    toggleDeepZoom(evt) {
+        // Switch between image and OSD, depending on which is currently active
+        const container = evt.target.parentNode.parentNode;
+        const OSD = container.querySelector(".openseadragon-container");
+        const image = container.querySelector("img.iiif-image");
+        if (OSD) {
+            container.removeChild(OSD);
+            image.style.display = "block";
+        } else {
+            this.activateDeepZoom(container, image);
+        }
+    }
+    activateDeepZoom(container, image) {
+        // hide image and add OpenSeaDragon to container
+        const height = container.getBoundingClientRect()["height"];
+        container.style.height = `${height}px`;
+        image.style.display = "none";
+        this.addOpenSeaDragon(container, [container.dataset.iiifUrl]);
+    }
+    addOpenSeaDragon(element, tileSources) {
+        // inject OSD into the image container
         OpenSeadragon({
-            id: this.iiifContainerTarget.id,
+            element,
             prefixUrl:
                 "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/3.0.0/images/",
-            tileSources: JSON.parse(this.iiifContainerTarget.dataset.iiifUrls),
-            sequenceMode: true,
-            preserveViewport: true,
-            autoHideControls: false,
+            tileSources,
+            sequenceMode: false,
+            autoHideControls: true,
             showHomeControl: false,
-            showRotationControl: true,
+            showZoomControl: false,
+            showNavigationControl: false,
+            showFullPageControl: false,
+            showSequenceControl: false,
             crossOriginPolicy: "Anonymous",
             gestureSettingsTouch: {
                 pinchRotate: true,
             },
         });
-    }
-    iiifContainerTargetDisconnected() {
-        // ensure OSD is removed on disconnect by removing all child nodes from target
-        while (this.iiifContainerTarget.firstChild) {
-            this.iiifContainerTarget.removeChild(
-                this.iiifContainerTarget.firstChild
-            );
-        }
     }
 }
