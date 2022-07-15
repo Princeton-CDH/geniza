779ab31c99288d423d9051cb0d4721d1a59cdb3e
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-07-15 10:50:06 -0400

    Make footnote content editable in admin

--- geniza/footnotes/admin.py
+++ geniza/footnotes/admin.py
@@ -352,6 +352,7 @@ class FootnoteAdmin(admin.ModelAdmin):
                     "doc_relation",
                     "url",
                     "notes",
+                    "content",
                 )
             },
         ),
