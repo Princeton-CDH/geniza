463b6794930dc18ef3a06fb06f888805c8b1da54
Ben Silverman <ben@performantsoftware.com>
2022-09-22 10:54:16 -0400

    Restore prior "select with disabled" functionality ref #1089
    Co-authored-by: rlskoeser <rebecca.s.koeser@princeton.edu>

--- geniza/corpus/forms.py
+++ geniza/corpus/forms.py
@@ -8,6 +8,40 @@ from geniza.common.utils import simplify_quotes
 from geniza.corpus.models import Document
 
 
+class SelectDisabledMixin:
+    """
+    Mixin for :class:`django.forms.RadioSelect` or :class:`django.forms.CheckboxSelect`
+    classes to set an option as disabled. To disable, the widget's choice
+    label option should be passed in as a dictionary with `disabled` set
+    to True::
+        {'label': 'option', 'disabled': True}.
+    """
+
+    # copied from mep-django
+
+    # Using a solution at https://djangosnippets.org/snippets/2453/
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        disabled = None
+
+        if isinstance(label, dict):
+            label, disabled = label["label"], label.get("disabled", False)
+        option_dict = super().create_option(
+            name, value, label, selected, index, subindex=subindex, attrs=attrs
+        )
+        if disabled:
+            option_dict["attrs"].update({"disabled": "disabled"})
+        return option_dict
+
+
+class SelectWithDisabled(SelectDisabledMixin, forms.Select):
+    """
+    Subclass of :class:`django.forms.Select` with option to mark
+    a choice as disabled.
+    """
+
+
 class CheckboxSelectWithCount(forms.CheckboxSelectMultiple):
     # extend default CheckboxSelectMultiple to add facet counts and
     # include per-item count as a data attribute
@@ -155,11 +189,9 @@ class DocumentSearchForm(RangeForm):
     sort = forms.ChoiceField(
         # Translators: label for form sort field
         label=_("Sort by"),
-        choices=[
-            (choice[0], mark_safe(f"<span>{choice[1]}</span>"))
-            for choice in SORT_CHOICES
-        ],
+        choices=SORT_CHOICES,
         required=False,
+        widget=SelectWithDisabled,
     )
     # Translators: label for filter documents by date range
     docdate = RangeField(
@@ -198,6 +230,19 @@ class DocumentSearchForm(RangeForm):
         "has_translation": "has_translation",
         "has_discussion": "has_discussion",
     }
+
+    def __init__(self, data=None, *args, **kwargs):
+        """
+        Override to set choices dynamically based on form kwargs.
+        """
+        super().__init__(data=data, *args, **kwargs)
+
+        # if a keyword search term is not present, relevance sort is disabled
+        if not data or not data.get("q", None):
+            self.fields["sort"].widget.choices[0] = (
+                self.SORT_CHOICES[0][0],
+                {"label": self.SORT_CHOICES[0][1], "disabled": True},
+            )
 
     def filters_active(self):
         """Check if any filters are active; returns true if form fields other than sort or q are set"""
--- geniza/corpus/tests/test_forms.py
+++ geniza/corpus/tests/test_forms.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock
 
 import pytest
+from django import forms
 
 from geniza.corpus.forms import (
     CheckboxSelectWithCount,
@@ -8,8 +9,31 @@ from geniza.corpus.forms import (
     DocumentMergeForm,
     DocumentSearchForm,
     FacetChoiceField,
+    SelectWithDisabled,
 )
 from geniza.corpus.models import Document
+
+
+class TestSelectedWithDisabled:
+    # test adapted from ppa-django/mep-django
+
+    class TestForm(forms.Form):
+        """Build a test form use the widget"""
+
+        CHOICES = (
+            ("no", {"label": "no select", "disabled": True}),
+            ("yes", "yes can select"),
+        )
+
+        yes_no = forms.ChoiceField(choices=CHOICES, widget=SelectWithDisabled)
+
+    def test_create_option(self):
+        form = self.TestForm()
+        rendered = form.as_p()
+        print(rendered)
+        # no is disabled
+        assert '<option value="no" disabled="disabled"' in rendered
+        assert '<option value="yes">' in rendered
 
 
 class TestFacetChoiceField:
@@ -33,6 +57,28 @@ class TestFacetChoiceField:
 
 class TestDocumentSearchForm:
     # test adapted from ppa-django
+
+    def test_init(self):
+        data = {"q": "illness"}
+        # has query, relevance enabled
+        form = DocumentSearchForm(data)
+        assert form.fields["sort"].widget.choices[0] == form.SORT_CHOICES[0]
+
+        # empty query, relevance disabled
+        data["q"] = ""
+        form = DocumentSearchForm(data)
+        assert form.fields["sort"].widget.choices[0] == (
+            "relevance",
+            {"label": "Relevance", "disabled": True},
+        )
+
+        # no query, also relevance disabled
+        del data["q"]
+        form = DocumentSearchForm(data)
+        assert form.fields["sort"].widget.choices[0] == (
+            "relevance",
+            {"label": "Relevance", "disabled": True},
+        )
 
     def test_choices_from_facets(self):
         """A facet dict should produce correct choice labels"""
