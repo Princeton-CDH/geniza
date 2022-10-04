db0b68c44649250d143b0a37d8c8ca3b4afb9f4f
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-10-04 17:08:28 -0400

    Handle transcription export for deleted annotations

--- geniza/corpus/management/commands/sync_annotation_export.py
+++ geniza/corpus/management/commands/sync_annotation_export.py
@@ -4,9 +4,10 @@ from collections import defaultdict
 from datetime import datetime
 
 from django.conf import settings
-from django.contrib.admin.models import LogEntry
+from django.contrib.admin.models import DELETION, LogEntry
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import pluralize
 from django.utils import timezone
 
 from geniza.annotations.models import Annotation
@@ -50,24 +51,48 @@ class Command(BaseCommand):
         new_lastrun = timezone.now()
 
         if options["verbosity"] >= self.v_normal:
-            print("%d annotation log entries since %s" % (log_entries.count(), lastrun))
+            log_entry_count = log_entries.count()
+            print(
+                "%d annotation log entr%s since %s"
+                % (
+                    log_entry_count,
+                    pluralize(log_entry_count, "y,ies"),
+                    lastrun,
+                )
+            )
 
         # generate exports based on what has been changed
         if log_entries.exists():
 
-            # TODO: handle deletions (object no longer in db)
-
-            # make a dictionary of modified annotations and the users who modified them
+            # make a dictionary of annotations and the users who modified them
             modified_annotations = defaultdict(list)
+            # also track any deletions
+            # needs special handling since annotation is no longer in db
+            deletions = []
+
             for log_entry in log_entries:
                 modified_annotations[log_entry.object_id].append(log_entry.user)
+                if log_entry.action_flag == DELETION:
+                    deletions.append(log_entry)
 
-            # load the modified annotations from the database
+            # load modified annotations from the database
             annotations = Annotation.objects.filter(
                 id__in=list(modified_annotations.keys())
             )
             # group by manifest, so we can export by document
             annos_by_manifest = annotations.group_by_manifest()
+
+            # special case: deleted annotations don't exist in the db,
+            # but the transcription should be re-exported to reflect remvoal
+            for log_entry in deletions:
+                # if modified via annotation delete view, change message
+                # should include manifest uri
+                manifest_uri = json.loads(log_entry.change_message).get("manifest_uri")
+                if manifest_uri:
+                    # add an unsaved annotation with the proper id to the dict
+                    annos_by_manifest[manifest_uri].append(
+                        Annotation(id=log_entry.object_id)
+                    )
 
             for manifest, annotations in annos_by_manifest.items():
                 # export transcription for the specified document,
