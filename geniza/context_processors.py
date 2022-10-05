2c4710f1f4535db5a49ac58b2c1483bf4e67005c
Ben Silverman <ben@performantsoftware.com>
2022-10-05 15:56:26 -0400

    Restore test banner and make configurable (#1150)

--- geniza/context_processors.py
+++ geniza/context_processors.py
@@ -9,7 +9,9 @@ def template_globals(request):
     site = Site.find_for_request(request)
     admin_language_codes = [lang[0] for lang in settings.LANGUAGES]
     context_extras = {
-        "SHOW_TEST_WARNING": getattr(settings, "SHOW_TEST_WARNING", False),
+        "FEATURE_FLAGS": getattr(settings, "FEATURE_FLAGS", []),
+        "TEST_WARNING_HEADING": getattr(settings, "TEST_WARNING_HEADING", None),
+        "TEST_WARNING_MESSAGE": getattr(settings, "TEST_WARNING_MESSAGE", None),
         "FONT_URL_PREFIX": getattr(settings, "FONT_URL_PREFIX", ""),
         "PUBLIC_SITE_LANGUAGES": getattr(
             settings, "PUBLIC_SITE_LANGUAGES", admin_language_codes
