f55710fe948d4e897e23049b8a531bc64f38512b
Ben Silverman <ben@performantsoftware.com>
2022-10-13 13:43:54 -0400

    Add transcription controller to IIIF bundle (#1202)

--- sitemedia/js/iiif.js
+++ sitemedia/js/iiif.js
@@ -3,5 +3,9 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
 const application = Application.start();
 // only require iiif_controller.js
-const context = require.context("./controllers", true, /iiif_controller\.js$/);
+const context = require.context(
+    "./controllers",
+    true,
+    /(iiif_controller|transcription_controller)\.js$/
+);
 application.load(definitionsFromContext(context));
