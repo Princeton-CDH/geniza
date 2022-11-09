55f482d831df4a638a69de46f56856bd1a8444d1
rlskoeser <rlskoeser@users.noreply.github.com>
2022-11-09 19:43:00 +0000

    deploy: 6f9d26424cffd4d9f821771bb5ed61816eabd4f8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.10.0',
+    VERSION: '4.10.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
