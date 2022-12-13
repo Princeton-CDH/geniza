0b54331c2e780a0a2a320b351bfd2a130f0b1eb1
Ben Silverman <ben@performantsoftware.com>
2022-12-13 11:51:36 -0500

    Increase annotorious handle radius

--- sitemedia/js/controllers/annotation_controller.js
+++ sitemedia/js/controllers/annotation_controller.js
@@ -65,7 +65,7 @@ export default class extends Controller {
 
         // enable annotorious-tahqiq
         const { config, canvasURL, manifestId, annotationContainer } = settings;
-        const anno = Annotorious(viewer);
+        const anno = Annotorious(viewer, { handleRadius: 10 });
 
         // Initialize the AnnotationServerStorage plugin
         const annotationServerConfig = {
