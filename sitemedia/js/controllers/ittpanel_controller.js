bbfa2d7f8a54e27df5717ae0752f6d8e673e1e36
Ben Silverman <ben@performantsoftware.com>
2023-06-01 17:09:37 -0400

    Align transcriptions/translations with JS (#1358)

--- sitemedia/js/controllers/ittpanel_controller.js
+++ sitemedia/js/controllers/ittpanel_controller.js
@@ -3,7 +3,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["toggle"];
+    static targets = ["toggle", "transcription", "translation"];
+
+    initialize() {
+        // bind "this" so we can access other methods in this controller from within event handler
+        this.boundAlertHandler = this.handleSaveAnnotation.bind(this);
+    }
+
+    connect() {
+        document.addEventListener("tahqiq-alert", this.boundAlertHandler);
+    }
+
+    disconnect() {
+        document.removeEventListener("tahqiq-alert", this.boundAlertHandler);
+    }
+
+    isDesktop() {
+        // Minimum width for desktop devices
+        return window.innerWidth >= 900;
+    }
 
     clickToggle(evt) {
         // when all three toggles are opened, automatically close one, depending on which you
@@ -26,6 +44,87 @@ export default class extends Controller {
                         (target) => target.id === "images-on"
                     ).checked = false;
                     break;
+            }
+        }
+
+        if (this.isDesktop()) {
+            if (this.transcriptionAndTranslationOpen()) {
+                // when transcription and translation are both opened, align their contents line-by-line
+                this.alignLines();
+            } else {
+                // when one of those two toggles is closed, remove inline styles from lines (alignment no longer needed)
+                this.transcriptionTarget
+                    .querySelectorAll("li")
+                    .forEach((li) => {
+                        li.removeAttribute("style");
+                    });
+                this.translationTarget.querySelectorAll("li").forEach((li) => {
+                    li.removeAttribute("style");
+                });
+            }
+        }
+    }
+
+    transcriptionAndTranslationOpen() {
+        // check toggle targets to find out whether both transcription and translated are open
+        return ["transcription-on", "translation-on"].every(
+            (id) =>
+                this.toggleTargets.find((target) => target.id === id)
+                    .checked === true
+        );
+    }
+
+    alignLines() {
+        // align each line of transcription to translation
+        const edLines = this.transcriptionTarget.querySelectorAll("li");
+        const trLines = this.translationTarget.querySelectorAll("li");
+        // only align as many lines as we need to
+        const [lessLines, moreLines] =
+            edLines.length < trLines.length
+                ? [trLines, edLines]
+                : [edLines, trLines];
+        lessLines.forEach((li, i) => {
+            if (li && moreLines[i]) {
+                // compare top of lines by position in list
+                const thisLiPos = li.getBoundingClientRect();
+                const thatLiPos = moreLines[i].getBoundingClientRect();
+                // add padding-top to the elements that need it
+                if (thisLiPos.top < thatLiPos.top) {
+                    li.style.paddingTop = `${thatLiPos.top - thisLiPos.top}px`;
+                } else if (thisLiPos.top > thatLiPos.top) {
+                    moreLines[i].style.paddingTop = `${
+                        thisLiPos.top - thatLiPos.top
+                    }px`;
+                }
+            }
+        });
+    }
+
+    clickAnnotation(e) {
+        // on mousedown to edit content, clear any style that was added to li elements.
+        // otherwise, modified HTML will be loaded inside the editor, and saved with the annotation!
+        if (this.isDesktop() && e.target.tagName === "ANNOTATION-BLOCK") {
+            const lines = this.transcriptionTarget.querySelector(
+                "annotation-block"
+            )
+                ? this.transcriptionTarget.querySelectorAll("li")
+                : this.translationTarget.querySelectorAll("li");
+            lines.forEach((li) => {
+                li.removeAttribute("style");
+            });
+        }
+    }
+
+    handleSaveAnnotation(e) {
+        // reverse of clickAnnotation; on save, re-align transcription and translation lines
+        // TODO: handle cancel?
+        if (this.isDesktop()) {
+            const { message } = e.detail;
+            if (
+                message.includes("saved") &&
+                this.transcriptionAndTranslationOpen()
+            ) {
+                this.alignLines();
             }
         }
     }
