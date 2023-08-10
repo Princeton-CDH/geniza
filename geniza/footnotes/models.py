6354eb3b17bc922d2b12721274fd2da33f37b4a4
Ben Silverman <ben@performantsoftware.com>
2023-08-10 11:57:12 -0400

    Respect annotation reordering in doc detail page ref #1438

--- geniza/footnotes/models.py
+++ geniza/footnotes/models.py
@@ -606,7 +606,9 @@ class Footnote(TrackChangesModel):
         # keyed on canvas uri
         # handle multiple annotations on the same canvas
         html_content = defaultdict(list)
-        for a in self.annotation_set.all():
+        for a in self.annotation_set.all().order_by(
+            "content__schema:position", "created"
+        ):
             if a.label:
                 html_content[a.target_source_id].append(f"<h3>{a.label}</h3>")
             html_content[a.target_source_id].append(a.body_content)
