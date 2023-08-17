cc0c3ce1e54686c60c2543120ddf876f666ad185
Ben Silverman <ben@performantsoftware.com>
2023-08-17 16:19:58 -0400

    Fix issues with alignment CSS/JS (#1409)

--- sitemedia/js/controllers/ittpanel_controller.js
+++ sitemedia/js/controllers/ittpanel_controller.js
@@ -59,29 +59,34 @@ export default class extends Controller {
                 // when transcription and translation are both opened, align their contents line-by-line
                 this.alignLines();
             } else {
-                // when one of those two toggles is closed, remove data-lines from each line (alignment no longer needed)
+                // when one of those two toggles is closed, remove data-lines from each line
+                // (alignment no longer needed)
                 if (this.hasTranscriptionTarget) {
-                    this.transcriptionTarget
-                        .querySelectorAll("li")
-                        .forEach((li) => {
+                    this.transcriptionTargets.forEach((target) => {
+                        target.querySelectorAll("li").forEach((li) => {
                             if (li) li.removeAttribute("data-lines");
                         });
+                    });
                 }
                 if (this.hasTranslationTarget) {
-                    this.translationTarget
-                        .querySelectorAll("li")
-                        .forEach((li) => {
+                    this.translationTargets.forEach((target) => {
+                        target.querySelectorAll("li").forEach((li) => {
                             if (li) li.removeAttribute("data-lines");
                         });
+                    });
                 }
                 // also remove padding-top alignment of the two lists
                 if (this.hasTranscriptionTarget) {
-                    const edOL = this.transcriptionTarget.querySelector("ol");
-                    if (edOL) edOL.removeAttribute("style");
+                    this.transcriptionTargets.forEach((target) => {
+                        const edOL = target.querySelector("ol");
+                        if (edOL) edOL.removeAttribute("style");
+                    });
                 }
                 if (this.hasTranslationTarget) {
-                    const trOL = this.translationTarget.querySelector("ol");
-                    if (trOL) trOL.removeAttribute("style");
+                    this.translationTargets.forEach((target) => {
+                        const trOL = target.querySelector("ol");
+                        if (trOL) trOL.removeAttribute("style");
+                    });
                 }
             }
         }
@@ -106,38 +111,45 @@ export default class extends Controller {
     }
 
     alignLines() {
-        // first, align tops of lists (using inline styles)
-        const edTop = this.transcriptionTarget
-            .querySelector("ol")
-            .getBoundingClientRect().top;
-        const trTop = this.translationTarget
-            .querySelector("ol")
-            .getBoundingClientRect().top;
-        if (edTop < trTop) {
-            this.transcriptionTarget.querySelector("ol").style.paddingTop = `${
-                trTop - edTop
-            }px`;
-        } else if (trTop < edTop) {
-            this.translationTarget.querySelector("ol").style.paddingTop = `${
-                edTop - trTop
-            }px`;
-        }
-
-        // then, align each line of transcription to translation
-        const edLines = this.transcriptionTarget.querySelectorAll("li");
-        const trLines = this.translationTarget.querySelectorAll("li");
-        // only align as many lines as we need to
-        const minLines = Math.min(edLines.length, trLines.length);
-        for (let i = 0; i < minLines; i++) {
-            if (edLines[i] && trLines[i]) {
-                // calculate number of lines based on line height
-                const maxLineCount = Math.max(
-                    this.getLineCount(edLines[i]),
-                    this.getLineCount(trLines[i])
-                );
-                // set data-lines attribute on each li according to which is longer
-                edLines[i].setAttribute("data-lines", maxLineCount);
-                trLines[i].setAttribute("data-lines", maxLineCount);
+        // loop through each transcription and translation (only as many as needed)
+        const minTargets = Math.min(
+            this.transcriptionTargets.length,
+            this.translationTargets.length
+        );
+        for (let i = 0; i < minTargets; i++) {
+            // loop through as many OLs in each transcription/translation as needed
+            const edOls = this.transcriptionTargets[i].querySelectorAll("ol");
+            const trOls = this.translationTargets[i].querySelectorAll("ol");
+            const minLists = Math.min(edOls.length, trOls.length);
+            for (let j = 0; j < minLists; j++) {
+                // first, align tops of lists (using inline styles)
+                const edOl = edOls[j];
+                const trOl = trOls[j];
+                const edTop = edOl.getBoundingClientRect().top;
+                // translation is always 1 pixel difference
+                const trTop = trOl.getBoundingClientRect().top - 1;
+                if (edTop < trTop) {
+                    edOl.style.paddingTop = `${trTop - edTop}px`;
+                } else if (trTop < edTop) {
+                    trOl.style.paddingTop = `${edTop - trTop}px`;
+                }
+                // then, align each line of transcription to translation
+                const edLines = edOl.querySelectorAll("li");
+                const trLines = trOl.querySelectorAll("li");
+                // only align as many lines as we need to
+                const minLines = Math.min(edLines.length, trLines.length);
+                for (let k = 0; k < minLines; k++) {
+                    if (edLines[k] && trLines[k]) {
+                        // calculate number of lines based on line height
+                        const maxLineCount = Math.max(
+                            this.getLineCount(edLines[k]),
+                            this.getLineCount(trLines[k])
+                        );
+                        // set data-lines attribute on each li according to which is longer
+                        edLines[k].setAttribute("data-lines", maxLineCount);
+                        trLines[k].setAttribute("data-lines", maxLineCount);
+                    }
+                }
             }
         }
     }
