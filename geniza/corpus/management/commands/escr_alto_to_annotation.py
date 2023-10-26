86a31ffcabcd5ec85dee3f3383bd83fc00ae5124
Ben Silverman <ben@performantsoftware.com>
2023-10-26 17:09:46 -0400

    First draft of escr ALTO XML to annotations

---
+++ geniza/corpus/management/commands/escr_alto_to_annotation.py
@@ -0,0 +1,252 @@
+import re
+
+from django.conf import settings
+from django.contrib.admin.models import ADDITION, LogEntry
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from djiffy.models import Canvas, Manifest
+from eulxml import xmlmap
+
+from geniza.annotations.models import Annotation
+from geniza.corpus.models import Document
+from geniza.footnotes.models import Footnote, Source, SourceType
+
+# parse eScriptorium ALTO files, generate transcription IIIF annotations,
+# and attach to correct documents
+
+
+class AltoObject(xmlmap.XmlObject):
+    # all XmlObjects should inherit this class to get the root namespace
+    ROOT_NAMESPACES = {"alto": "http://www.loc.gov/standards/alto/ns-v4#"}
+
+
+class AltoPolygonalObject(AltoObject):
+    polygon = xmlmap.NodeField("alto:Shape/alto:Polygon/@POINTS", AltoObject)
+
+
+class Line(AltoPolygonalObject):
+    content = xmlmap.StringField("alto:String/@CONTENT")
+
+
+class TextBlock(AltoPolygonalObject):
+    id = xmlmap.StringField("./@ID")
+    lines = xmlmap.NodeListField("alto:TextLine", Line)
+    block_type_id = xmlmap.StringField("./@TAGREFS")
+
+
+class PrintSpace(AltoObject):
+    textblocks = xmlmap.NodeListField("alto:TextBlock", TextBlock)
+
+
+class EscriptoriumAlto(AltoObject):
+    filename = xmlmap.StringField(
+        "//alto:alto/alto:Description/alto:sourceImageInformation/alto:fileName"
+    )
+    printspace = xmlmap.NodeField(
+        "//alto:alto/alto:Layout/alto:Page/alto:PrintSpace", PrintSpace
+    )
+    tags = xmlmap.NodeListField("//alto:alto/alto:Tags/alto:OtherTag", AltoObject)
+
+
+class Command(BaseCommand):
+    # regex pattern for image filenames
+    filename_pattern = r"PGPID_(?P<pgpid>\d+)_(?P<shelfmark>[\w\-]+)_(?P<img>\d)\..+"
+
+    def add_arguments(self, parser):
+        # needs xml filenames as input
+        parser.add_argument(
+            "alto", metavar="ALTOXML", nargs="+", help="ALTO files to be processed"
+        )
+
+    def handle(self, *args, **options):
+        self.script_user = User.objects.get(username=settings.SCRIPT_USERNAME)
+
+        for xmlfile in options["alto"]:
+            print("Processing %s" % xmlfile)
+            alto = xmlmap.load_xmlobject_from_file(xmlfile, EscriptoriumAlto)
+            # associate filename with pgpid
+            m = re.match(self.filename_pattern, alto.filename)
+            pgpid = int(m.group("pgpid"))
+            try:
+                doc = Document.objects.get(
+                    Q(id=pgpid) | Q(old_pgpids__contains=[pgpid])
+                )
+            except Document.DoesNotExist:
+                print("Could not match document; skipping")
+                continue
+
+            # we should be able to match the shelfmark portion to a manifest short_id
+            manifest = self.get_manifest(doc, m.group("shelfmark"))
+
+            # use canvas short_id = img number in sequence
+            img_number = int(m.group("img")) + 1
+            canvas = self.get_canvas(manifest, img_number)
+
+            if canvas:
+                canvas_uri = canvas.uri
+            else:
+                # create a placeholder canvas URI that contains textblock pk and canvas number
+                canvas_base_uri = "%siiif/" % doc.permalink
+                b = doc.textblock_set.first()
+                canvas_uri = f"{canvas_base_uri}textblock/{b.pk}/canvas/{img_number}/"
+
+            # get scale factor for converting textblock geometry, based on full image width
+            img_width = alto.printspace.node.attrib["WIDTH"]
+            scale_factor = int(img_width) / (
+                canvas.image.image_width if canvas else 640  # placeholder width = 640
+            )
+
+            # create annotations
+            for tb in alto.printspace.textblocks:
+                # associate tag with tagrefs to get block type
+                tags = [tag.node.attrib for tag in alto.tags]
+                if tb.block_type_id:
+                    tag = next((t for t in tags if t["ID"] == tb.block_type_id), None)
+                    block_type = tag["LABEL"]
+                    # TODO: When implementing line-by-line, use block_type to determine rotation
+
+                # skip arabic; these are Hebrew script transcriptions
+                if not (block_type and "Arabic" in block_type) and len(tb.lines):
+                    # get or create footnote
+                    footnote = self.get_footnote(doc)
+                    # create annotation and log entry
+                    anno = Annotation.objects.create(
+                        content=self.create_block_annotation(
+                            tb, canvas_uri, scale_factor
+                        ),
+                        footnote=footnote,
+                    )
+                    LogEntry.objects.log_action(
+                        user_id=self.script_user.pk,
+                        content_type_id=ContentType.objects.get_for_model(
+                            Annotation
+                        ).pk,
+                        object_id=anno.pk,
+                        object_repr=str(anno),
+                        change_message="Migrated from eScriptorium HTR ALTO",
+                        action_flag=ADDITION,
+                    )
+
+    def get_manifest(self, document, short_id):
+        """Attempt to get the manifest using the supplied short id; fallback to first manifest,
+        or return None if there are none on the document"""
+        try:
+            # NOTE: this works in 100% of tested files so far!
+            return Manifest.objects.get(short_id=short_id)
+        except Manifest.DoesNotExist:
+            print(
+                f"Could not find manifest with short_id {short_id}, falling back to first "
+                + "manifest on document"
+            )
+            manifests = [b.fragment.manifest for b in document.textblock_set.all()]
+            # associate with the first manifest
+            if manifests and manifests[0]:
+                return manifests[0]
+            else:
+                print(
+                    "Could not find manifests on document, falling back to placeholder "
+                    + "canvases"
+                )
+                return None
+
+    def get_canvas(self, manifest, img_number):
+        """Attempt to get a canvas from a manifest by id; fallback to first canvas, or return
+        None if there are none in the manifest (or there is no manifest)"""
+        if manifest and manifest.canvases.exists():
+            try:
+                return manifest.canvases.get(short_id=str(img_number))
+            except Canvas.DoesNotExist:
+                print(
+                    f"Could not find canvas with short_id {img_number}, falling back to "
+                    + "first canvas"
+                )
+                return manifest.canvases.first()
+        else:
+            return None
+
+    def get_footnote(self, document):
+        """Get or create a digital edition footnote for the HTR transcription"""
+        # TODO: Replace this with desired source type and source after decision is made
+        (model, _) = SourceType.objects.get_or_create(type="Machine learning model")
+        (source, _) = Source.objects.get_or_create(
+            title_en="eScriptorium HTR model",
+            source_type=model,
+        )
+        try:
+            return Footnote.objects.get(
+                doc_relation__contains=Footnote.DIGITAL_EDITION,
+                source__pk=source.pk,
+                content_type=ContentType.objects.get_for_model(Document),
+                object_id=document.pk,
+            )
+        except Footnote.DoesNotExist:
+            footnote = Footnote.objects.create(
+                content_object=document,
+                source=source,
+                doc_relation=[Footnote.DIGITAL_EDITION],
+            )
+            LogEntry.objects.log_action(
+                user_id=self.script_user.pk,
+                content_type_id=ContentType.objects.get_for_model(Footnote).pk,
+                object_id=footnote.pk,
+                object_repr=str(footnote),
+                change_message="Created new footnote for eScriptorium digital edition",
+                action_flag=ADDITION,
+            )
+            return footnote
+
+    def scale_polygon(self, polygon, scale):
+        """Scale points in ALTO polygon by specified scale factor"""
+        points = str(polygon).split(" ")
+        scaled_points = [round(int(point) / scale, 4) for point in points]
+        # return as string for use in svg polygon element
+        return " ".join([str(point) for point in scaled_points])
+
+    def create_block_annotation(self, textblock, canvas_uri, scale_factor):
+        """Produce a valid IIIF annotation with the block-level content and geometry,
+        linked to the IIIF canvas by URI"""
+
+        # lines to HTML list
+        block_text = "<ol>\n"
+        for line in textblock.lines:
+            block_text += f"<li>{line.content}</li>\n"
+        block_text += "</ol>"
+
+        # create IIIF annotation
+        anno_content = {}
+        anno_content["body"] = [
+            {
+                "TextInput": "rtl",
+                "format": "text/html",
+                "type": "TextualBody",
+                "value": block_text,
+            }
+        ]
+        anno_content["motivation"] = ["sc:supplementing", "transcribing"]
+        anno_content["target"] = {
+            "source": {
+                "id": canvas_uri,
+                "type": "Canvas",
+            },
+        }
+
+        # add selector
+        if textblock.polygon:
+            # scale polygon points and use SvgSelector
+            points = self.scale_polygon(textblock.polygon, scale_factor)
+            anno_content["target"]["selector"] = {
+                "type": "SvgSelector",
+                "value": f'<svg><polygon points="{points}"></polygon></svg>',
+            }
+        else:
+            print(f"No block-level geometry available for {textblock.id}")
+            # when no block-level geometry available, use full image FragmentSelector
+            anno_content["target"]["selector"] = {
+                "conformsTo": "http://www.w3.org/TR/media-frags/",
+                "type": "FragmentSelector",
+                "value": "xywh=percent:1,1,98,98",
+            }
+
+        return anno_content
---
+++ geniza/corpus/tests/test_escr_alto_to_annotation.py
@@ -0,0 +1,122 @@
+import os.path
+from unittest.mock import patch
+
+from django.conf import settings
+from django.contrib.admin.models import ADDITION, LogEntry
+from django.contrib.auth.models import User
+from djiffy.models import Canvas
+from eulxml import xmlmap
+
+from geniza.corpus.management.commands.escr_alto_to_annotation import (
+    Command,
+    EscriptoriumAlto,
+)
+from geniza.corpus.models import Document
+from geniza.footnotes.models import Footnote
+
+fixture_dir = os.path.join(os.path.dirname(__file__), "fixtures")
+
+xmlfile = os.path.join(fixture_dir, "PGPID_6032_MS-TS-AS-00152-00383_0.xml")
+
+
+class TestEscrToAltoAnnotation:
+    cmd = Command()
+
+    def test_fields(self):
+        alto = xmlmap.load_xmlobject_from_file(xmlfile, EscriptoriumAlto)
+        assert "PGPID_6032" in alto.filename
+        assert len(alto.tags) == 18
+        assert alto.printspace
+        assert len(alto.printspace.textblocks) == 1
+        tb = alto.printspace.textblocks[0]
+        assert "eSc_textblock_" in tb.id
+        assert tb.block_type_id == "BT2"
+        assert tb.polygon
+        assert len(tb.lines) == 14
+        line = tb.lines[0]
+        assert line.polygon
+        assert line.content == "חטל אללה בקאך נ["
+
+    def test_scale_polygon(self):
+        alto = xmlmap.load_xmlobject_from_file(xmlfile, EscriptoriumAlto)
+        block = alto.printspace.textblocks[0]
+        # some of the polygon points that will be scaled
+        assert "1663 319 1668 391 1648 424" in str(block.polygon)
+
+        # should divide each number by the scale factor
+        scaled_polygon = self.cmd.scale_polygon(block.polygon, 0.5)
+        assert f"{1663/0.5} {319/0.5} {1668/0.5} {391/0.5} {1648/0.5} {424/0.5}" in str(
+            scaled_polygon
+        )
+
+        # if scale factor produces irrational numbers, should round to 4 decimal places
+        scaled_polygon = self.cmd.scale_polygon(block.polygon, 3)
+        assert str(1663 / 3) not in str(scaled_polygon)
+        assert str(round(1663 / 3, 4)) in str(scaled_polygon)
+
+    def test_create_block_annotation(self):
+        alto = xmlmap.load_xmlobject_from_file(xmlfile, EscriptoriumAlto)
+        block = alto.printspace.textblocks[0]
+        with patch.object(self.cmd, "scale_polygon") as scale_mock:
+            scale_mock.return_value = "100 200"
+            anno_content = self.cmd.create_block_annotation(block, "mock_canvas", 2)
+
+            # anno body should contain the transcription text
+            assert "<li>חטל אללה בקאך נ[</li>" in anno_content["body"][0]["value"]
+
+            # anno target source should be canvas
+            assert anno_content["target"]["source"]["id"] == "mock_canvas"
+
+            # selector should be svg containing mock result polygon points
+            assert (
+                '<svg><polygon points="100 200">'
+                in anno_content["target"]["selector"]["value"]
+            )
+
+        # block without polygon should cover most of entire image with fragmentselector
+        del block.polygon
+        anno_content = self.cmd.create_block_annotation(block, "mock_canvas", 2)
+        assert anno_content["target"]["selector"]["value"] == "xywh=percent:1,1,98,98"
+
+    def test_get_manifest(self, document):
+        manifests = [b.fragment.manifest for b in document.textblock_set.all()]
+        id = manifests[0].short_id
+        # short id matches, should get manifest
+        assert self.cmd.get_manifest(document, id).pk == manifests[0].pk
+
+        # short id doesn't match, should get first manifest on document
+        assert self.cmd.get_manifest(document, None).pk == manifests[0].pk
+
+        # no manifests on document, no short id match, should return None
+        doc_2 = Document.objects.create()
+        assert self.cmd.get_manifest(doc_2, None) is None
+
+    def test_get_canvas(self, document):
+        manifests = [b.fragment.manifest for b in document.textblock_set.all()]
+        id = "test"
+        canvas = Canvas.objects.create(
+            short_id=id,
+            manifest=manifests[0],
+            label="fake image",
+            iiif_image_id="http://example.co/iiif/ts-1/00001",
+            order=1,
+        )
+        # short id matches, should get canvas
+        assert self.cmd.get_canvas(manifests[0], id).pk == canvas.pk
+
+        # short id doesn't match, should get first canvas on manifest
+        assert self.cmd.get_canvas(manifests[0], None).pk == canvas.pk
+
+        # no manifest, should return None
+        assert self.cmd.get_canvas(None, id) is None
+
+    def test_get_footnote(self, document):
+        self.cmd.script_user = User.objects.get(username=settings.SCRIPT_USERNAME)
+
+        # footnote does not exist, should create and log
+        fn = self.cmd.get_footnote(document)
+        assert fn.object_id == document.pk
+        assert LogEntry.objects.filter(object_id=fn.pk, action_flag=ADDITION).exists()
+
+        # footnote already exists, should find it
+        assert self.cmd.get_footnote(document).pk == fn.pk
