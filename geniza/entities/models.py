002d3c5fe8404f397b0fb818cb632ca192682564
Ben Silverman <ben@performantsoftware.com>
2025-02-24 14:36:04 -0500

    Show region icons on map (#1748)

--- geniza/entities/models.py
+++ geniza/entities/models.py
@@ -1561,6 +1561,7 @@ class Place(ModelIndexable, SlugMixin, PermalinkMixin):
                 # related object counts
                 "documents_i": self.documentplacerelation_set.count(),
                 "people_i": self.personplacerelation_set.count(),
+                "is_region_b": self.is_region,
             }
         )
         return index_data
@@ -1598,6 +1599,7 @@ class PlaceSolrQuerySet(AliasedSolrQuerySet):
         "documents": "documents_i",
         "people": "people_i",
         "location": "location_p",
+        "is_region": "is_region_b",
     }
 
 
