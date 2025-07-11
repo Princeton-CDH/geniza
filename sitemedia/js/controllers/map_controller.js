1f466adfa4ab934a18d0870cef10277b7b5368b0
Ben Silverman <ben@performantsoftware.com>
2025-07-11 13:23:47 -0400

    Add timeline ticks and labels (#1820)

--- sitemedia/js/controllers/map_controller.js
+++ sitemedia/js/controllers/map_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
         "marker",
         "placeList",
         "placeMarkers",
+        "timelineTicks",
     ];
 
     async getPlaces(url, sort, query, date_range) {
@@ -181,5 +182,96 @@ export default class extends Controller {
             element.scrollIntoView({ behavior: "smooth" });
         }
         element.classList.add("selected-place");
+    }
+    generateTicks(min, max, interval) {
+        // produce evenly spaced integers between min and max, inclusive, at the given interval
+        const start = Math.ceil(min / interval) * interval;
+        const end = Math.floor(max / interval) * interval;
+        const count = Math.floor((end - start) / interval) + 1;
+        return Array.from({ length: count }, (_, i) => start + i * interval);
+    }
+    debounce(f, delay) {
+        // simple debounce for timeline resize observer
+        let timer = 0;
+        return function (...args) {
+            clearTimeout(timer);
+            timer = setTimeout(() => f.apply(this, args), delay);
+        };
+    }
+    timelineTicksTargetConnected() {
+        // set up timeline constants
+        let { min, max } = this.timelineTicksTarget.dataset;
+        min = parseInt(min);
+        max = parseInt(max);
+        this.ticksMinSpacing = 45;
+        this.thumbPadding = 8;
+        this.svgNs = "http://www.w3.org/2000/svg";
+        const yearsPerTick = 50;
+        this.ticks = this.generateTicks(min, max, yearsPerTick);
+
+        // resize observer for window horizontal size change
+        this.timelineResizeObserver = new ResizeObserver(
+            this.debounce(() => this.recalculateTicks(), 50)
+        );
+        this.timelineResizeObserver.observe(this.timelineTicksTarget);
+
+        // calculate and position ticks on timeline
+        this.recalculateTicks();
+    }
+    recalculateTicks() {
+        // regenerate ticks and append to svg in window
+        // get timeline width minus thumb padding
+        const tlStart = this.thumbPadding;
+        const width = this.timelineTicksTarget.getBoundingClientRect().width;
+        const tlEnd = width - this.thumbPadding;
+
+        // remove all svg contents
+        const svg = this.timelineTicksTarget.querySelector("svg");
+        svg.replaceChildren();
+
+        // create the timeline baseline
+        const timelineBg = document.createElementNS(this.svgNs, "line");
+        timelineBg.setAttribute("x1", tlStart);
+        timelineBg.setAttribute("y1", 0);
+        timelineBg.setAttribute("x2", tlEnd);
+        timelineBg.setAttribute("y2", 0);
+        timelineBg.setAttribute("stroke", "var(--timeline-bg)");
+        svg.appendChild(timelineBg);
+
+        // get xOffsets for ticks based on timeline width
+        let { min, max } = this.timelineTicksTarget.dataset;
+        min = parseInt(min);
+        max = parseInt(max);
+        const ticks = this.ticks.map((tick) => ({
+            value: tick,
+            xOffset: tlStart + ((tick - min) / (max - min)) * (tlEnd - tlStart),
+        }));
+
+        // generate visible tick marks, skipping over those less than ticksMinSpacing away
+        let prevXOffset = 0;
+        ticks.forEach((tick, i) => {
+            if (i == 0 || tick.xOffset - prevXOffset > this.ticksMinSpacing) {
+                prevXOffset = tick.xOffset;
+                // create tick line
+                const line = document.createElementNS(this.svgNs, "line");
+                line.setAttribute("x1", tick.xOffset);
+                line.setAttribute("y1", -8);
+                line.setAttribute("x2", tick.xOffset);
+                line.setAttribute("y2", 8);
+                line.setAttribute("stroke", "var(--timeline-bg)");
+                svg.appendChild(line);
+
+                // create tick text (year)
+                const text = document.createElementNS(this.svgNs, "text");
+                text.setAttribute("x", tick.xOffset);
+                text.setAttribute("y", -20);
+                text.setAttribute("text-anchor", "middle");
+                text.setAttribute("font-size", "16px");
+                text.setAttribute("fill", "var(--timeline-fg)");
+
+                text.textContent = tick.value;
+                svg.appendChild(text);
+            }
+        });
     }
 }
